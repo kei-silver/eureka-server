58f30d1025d12ea3043a8d6a31174c39736ae31c
kei-silver <ruddms8124@daum.net>
2023-10-09 15:42:13 +0900

    init

---
+++ src/test/kotlin/com/chat/eurekaAndHystrix/EurekaAndHystrixApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chat.eurekaAndHystrix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaAndHystrixApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
